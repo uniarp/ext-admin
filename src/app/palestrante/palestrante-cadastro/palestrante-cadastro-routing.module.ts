d6118053f11f47a35ad1ef696d7d890632d3a460
Gabriel Soares <gabriel.mello2016@gmail.com>
2020-02-21 21:49:27 -0300

    ajustado modulo palestrante-cadastro

--- src/app/palestrante/palestrante-cadastro/palestrante-cadastro-routing.module.ts
+++ src/app/palestrante/palestrante-cadastro/palestrante-cadastro-routing.module.ts
@@ -1,12 +1,12 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
-import { CadastroPage } from './cadastro.page';
+import { PalestranteCadastroPage } from './palestrante-cadastro.page';
 
 const routes: Routes = [
   {
     path: '',
-    component: CadastroPage
+    component: PalestranteCadastroPage
   }
 ];
 
