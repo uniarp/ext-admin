ac2ef0fc85be3350bdd56cb0bce1f6e949b9e72f
MATHEUS DE OLIVEIRA <026462@uniarp.edu.br>
2020-02-28 21:34:50 -0300

    listagem de participante concluida

--- src/app/participante-pesquisa/participante-pesquisa.page.ts
+++ src/app/participante-pesquisa/participante-pesquisa.page.ts
@@ -7,9 +7,15 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ParticipantePesquisaPage implements OnInit {
 
+  participante = [
+    { id: 1, nome: 'Zorzo', email: 'zorzo@uniarp.com.br', cpf: '111.222.333-44'},
+    { id: 2, nome: 'Conte', email: 'zorzo@uniarp.com.br', cpf: '111.222.333-44'},
+  ];
+
   constructor() { }
 
   ngOnInit() {
+    console.log(this.participante);
   }
 
 }
