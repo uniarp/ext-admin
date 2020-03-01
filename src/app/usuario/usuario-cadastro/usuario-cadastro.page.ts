beb324a2196a6e93a138709dc6869905a7b12703
Gabriel Soares <gabriel.mello2016@gmail.com>
2020-03-01 10:03:57 -0300

    Implementado Pagina de Cadastro  de usuário

--- src/app/usuario/usuario-cadastro/usuario-cadastro.page.ts
+++ src/app/usuario/usuario-cadastro/usuario-cadastro.page.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormControl } from '@angular/forms';
 
 @Component({
   selector: 'app-usuario-cadastro',
@@ -7,9 +8,22 @@ import { Component, OnInit } from '@angular/core';
 })
 export class UsuarioCadastroPage implements OnInit {
 
+  usuario = { id: 0, nome: '', email: '', cpf: '', profissao: '', senha: '' };
+  confirSenha = '';
+  password_type = 'password';
+  profissao = ['Analista', 'Desenvolvedor', 'Consultor', 'Professor'];
   constructor() { }
 
   ngOnInit() {
+
+  }
+
+  exibeSenha() {
+    this.password_type = this.password_type === 'text' ? 'password' : 'text';
+  }
+
+  gravar(form: FormControl) {
+    console.log(this.usuario);
   }
 
 }
