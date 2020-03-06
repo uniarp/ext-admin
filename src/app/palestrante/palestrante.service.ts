d948a706a5847ab55ec89fffd65caf0e21cd43ba
berezanski <berezanskironaldo@gmail.com>
2020-03-06 15:30:56 -0300

    Palestrante-Pesquisa

--- src/app/palestrante/palestrante.service.ts
+++ src/app/palestrante/palestrante.service.ts
@@ -1,9 +1,42 @@
-import { Injectable } from '@angular/core';
+import { Injectable, OnInit } from '@angular/core';
+import { Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
-export class PalestranteService {
+export class PalestranteService implements OnInit {
+  private validou: Boolean;
+  private palestrantes: Palestrante[];
+  constructor() {
+    validou: true;
+    this.palestrantes = [{ id: '1', nome: 'Zorzo', cpf: '111.222.333-44', telefone: '(49) 99999-9999', email: 'zorzo@uniarp.com.br', area: 'sistemaas de Informação' },
+    { id: '2', nome: 'Conte', cpf: '111.222.333-44', telefone: '(49) 99999-9999', email: 'zorzo@uniarp.com.br', area: 'sistemaas de Informação' }];
+  }
+
+  ngOnInit() {
+
+  }
+
+  public removerPalestrante(idPal: string): Boolean {
+    return this.validou;
+  }
+
+  public adicionarPalestrante(palestrante: Palestrante): Boolean {
+    return this.validou;
+  }
+  public listarPalestrantes(): Palestrante[] {
+    return this.palestrantes;
+  }
+}
+
+export class Palestrante {
+  id: string;
+  nome: string;
+  cpf: string;
+  telefone: string;
+  email: string;
+  area: string;
 
   constructor() { }
+
 }
