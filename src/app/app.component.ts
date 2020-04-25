43f8bebdda02815afdf7c52cf0831903ed59325c
well-7 <welinton.7@hotmail.com>
2020-04-25 18:57:14 -0300

    criado side menu

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,6 +3,7 @@ import { Component } from '@angular/core';
 import { Platform } from '@ionic/angular';
 import { SplashScreen } from '@ionic-native/splash-screen/ngx';
 import { StatusBar } from '@ionic-native/status-bar/ngx';
+import { Router, RouterEvent } from '@angular/router';
 
 @Component({
   selector: 'app-root',
@@ -10,12 +11,38 @@ import { StatusBar } from '@ionic-native/status-bar/ngx';
   styleUrls: ['app.component.scss']
 })
 export class AppComponent {
+
+  pages = [
+    {
+      titulo: 'Gestão de Eventos',
+      url: '/evento-pesquisa'
+    },
+    {
+      titulo: 'Gestão de Usuários',
+      url: '/usuario-pesquisa'
+    },
+    {
+      titulo: 'Gestão de Palestrantes',
+      url: '/palestrante-pesquisa'
+    },
+    {
+      titulo: 'Gestão de Voluntários',
+      url: '/voluntario-pesquisa'
+    }
+  ];
+
+  selectedPath = '';
+
   constructor(
     private platform: Platform,
     private splashScreen: SplashScreen,
-    private statusBar: StatusBar
+    private statusBar: StatusBar,
+    private router: Router
   ) {
     this.initializeApp();
+    this.router.events.subscribe((event: RouterEvent) => {
+      this.selectedPath = event.url;
+    });
   }
 
   initializeApp() {
